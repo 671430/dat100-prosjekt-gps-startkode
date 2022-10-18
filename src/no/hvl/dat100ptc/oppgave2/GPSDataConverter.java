6f157c6f5cccfb549287bd5a519836d7bda373fe
Vegard Aa Albretsen <111348081+h599002@users.noreply.github.com>
2022-10-18 10:11:40 +0200

    Update GPSDataConverter.java

--- src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
+++ src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
@@ -2,39 +2,44 @@ package no.hvl.dat100ptc.oppgave2;
 
 import no.hvl.dat100ptc.TODO;
 import no.hvl.dat100ptc.oppgave1.GPSPoint;
+import static java.lang.Integer.*;
+import static java.lang.Double.*;
 
 public class GPSDataConverter {
-
-	// konverter tidsinformasjon i gps data punkt til antall sekunder fra midnatt
-	// dvs. ignorer information om dato og omregn tidspunkt til sekunder
-	// Eksempel - tidsinformasjon (som String): 2017-08-13T08:52:26.000Z
-    // skal omregnes til sekunder (som int): 8 * 60 * 60 + 52 * 60 + 26 
 	
-	private static int TIME_STARTINDEX = 11; // posisjon for start av tidspunkt i timestr
+	//private static int TIME_STARTINDEX = 11; // posisjon for start av tidspunkt i timestr
 
 	public static int toSeconds(String timestr) {
 		
 		int secs;
 		int hr, min, sec;
 		
-		// TODO
-		// OPPGAVE - START
+		int indexT = timestr.indexOf("T");
+		int indexPkt = timestr.indexOf(".");
 		
-		throw new UnsupportedOperationException(TODO.method());
-
-		// OPPGAVE - SLUTT
+		String str = timestr.substring((indexT + 1), indexPkt);
+		hr = parseInt(str.substring(0,2));
+		min = parseInt(str.substring(3,5));
+		sec = parseInt(str.substring(6,8));
+		
+		secs = hr*3600 + min*60 + sec;
+		
+		return secs;
 		
 	}
 
 	public static GPSPoint convert(String timeStr, String latitudeStr, String longitudeStr, String elevationStr) {
 
-		GPSPoint gpspoint;
-
-		// TODO - START ;
+		int time;
+		double latitude, longitude, elevation;
+		time = toSeconds(timeStr);
+		latitude = parseDouble(latitudeStr);
+		longitude = parseDouble(longitudeStr);
+		elevation = parseDouble(elevationStr);
 		
-		throw new UnsupportedOperationException(TODO.method());
-
-		// OPPGAVE - SLUTT ;
+		GPSPoint gpspoint = new GPSPoint(time,latitude,longitude,elevation);
+		
+		return gpspoint;
 	    
 	}
 	
